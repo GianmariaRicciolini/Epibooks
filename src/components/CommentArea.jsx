39cc67a8aaf6831942e9d8a948c7eb154240d2da
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-03-19 16:32:16 +0100

    booklist e commentArea update

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,85 +1,55 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import React, { useState, useEffect } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    isLoading: false,
-    isError: false,
-  }
+const CommentArea = (props) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  // componentDidMount = async () => {
-  //   try {
-  //     let response = await fetch(
-  //       'https://striveschool-api.herokuapp.com/api/comments/' +
-  //         this.props.asin,
-  //       {
-  //         headers: {
-  //           Authorization:
-  //             'Bearer inserisci-qui-il-tuo-token',
-  //         },
-  //       }
-  //     )
-  //     console.log(response)
-  //     if (response.ok) {
-  //       let comments = await response.json()
-  //       this.setState({ comments: comments, isLoading: false, isError: false })
-  //     } else {
-  //       console.log('error')
-  //       this.setState({ isLoading: false, isError: true })
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //     this.setState({ isLoading: false, isError: true })
-  //   }
-  // }
-
-  componentDidUpdate = async (prevProps) => {
-    if (prevProps.asin !== this.props.asin) {
-      this.setState({
-        isLoading: true,
-      })
+  useEffect(() => {
+    const fetchComments = async () => {
+      setIsLoading(true);
+      setIsError(false);
       try {
-        let response = await fetch(
-          'https://striveschool-api.herokuapp.com/api/comments/' +
-            this.props.asin,
-          {
-            headers: {
-              Authorization: 'Bearer inserisci-qui-il-tuo-token',
-            },
-          }
-        )
-        console.log(response)
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + props.asin, {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWUxYTFjNjRjNTllYzAwMTk5MGQ3MTUiLCJpYXQiOjE3MTA0MjY0NTAsImV4cCI6MTcxMTYzNjA1MH0.by3gpOjiSG1rajbrasJA7RWdqU93EFKfo8mZ0UrnIn8",
+          },
+        });
         if (response.ok) {
-          let comments = await response.json()
-          this.setState({
-            comments: comments,
-            isLoading: false,
-            isError: false,
-          })
+          const fetchedComments = await response.json();
+          setComments(fetchedComments);
+          setIsLoading(false);
         } else {
-          this.setState({ isLoading: false, isError: true })
+          setIsLoading(false);
+          setIsError(true);
         }
       } catch (error) {
-        console.log(error)
-        this.setState({ isLoading: false, isError: true })
+        console.log(error);
+        setIsLoading(false);
+        setIsError(true);
       }
+    };
+
+    if (props.asin) {
+      fetchComments();
+    } else {
+      setComments([]);
     }
-  }
+  }, [props.asin]);
 
-  render() {
-    return (
-      <div className="text-center">
-        {this.state.isLoading && <Loading />}
-        {this.state.isError && <Error />}
-        <AddComment asin={this.props.asin} />
-        <CommentList commentsToShow={this.state.comments} />
-      </div>
-    )
-  }
-}
+  return (
+    <div className="text-center">
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={props.asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
 
-export default CommentArea
+export default CommentArea;
